6df78e3d217b0429223f0e7a8f8c9155d92cc3f1
Abhishek Patel <abhidgreat8394@gmail.com>
2017-10-27 02:59:14 -0700

    Image to ASCII conversion complete
    - Convert image to ASCII
    - Scaling Optimization at table creation time

--- src/converter.py
+++ src/converter.py
@@ -6,6 +6,7 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 import argparse
 import json
+from math import ceil
 
 # meta data keys
 I2FILE_KEY = "i2file"
@@ -18,6 +19,8 @@ MIN_VAL_KEY = "min_brightness"
 MAX_VAL_KEY = "max_brightness"
 DATA_FOLDER_KEY = "src_data_folder"
 META_KEY = "meta"
+IMAGE_WIDTH_KEY = "char_width"
+IMAGE_HEIGHT_KEY = "char_height"
 
 def generate_char_image(font_file, chr_code, font_size, img_dimensions, bitmap_out_dir, should_save=True):
     """Generate a png image for a character.
@@ -33,11 +36,12 @@ def generate_char_image(font_file, chr_code, font_size, img_dimensions, bitmap_o
     Returns:
         (PIL.Image, string) : Image created along with its save file name
     """
-    chr_symbol = chr(chr_code)
+    # use latin-1 encoding to support some "extended ascii"
+    chr_symbol = chr(chr_code).encode("latin-1").decode("latin-1")
     image = Image.new("RGB", (img_dimensions,img_dimensions), (0,0,0)) #can do rgba too but not needed
     font = ImageFont.truetype(font_file, font_size)
     draw = ImageDraw.Draw(image)
-    draw.text((10, 0), chr_symbol, (255,255,255), font=font)
+    draw.text((5, 0), chr_symbol, (255,255,255), font=font)
     # to make image crisper
     img_resized = image.resize((img_dimensions//2,img_dimensions//2))
     save_file_name = "_".join(["ascii",str(chr_code),str(font_size)]) + ".png"
@@ -97,6 +101,9 @@ def avg_brightness_pixel(pixel):
     Raises:
         Exception: If not a RGB or RGBA pixel
     """
+    # 8-bit pixel.
+    if type(pixel) == int:
+        return pixel
     # rgb
     if len(pixel) == 3:
         return sum(pixel) / len(pixel)
@@ -106,7 +113,7 @@ def avg_brightness_pixel(pixel):
         return sum(pixel[:-1])/3 + pixel[-1]/255
     raise Exception("provide a RGB or RGBA tuple")
 
-def avg_brightness_calculator(pix, w, h):
+def avg_brightness_calculator(pix, w, h, row_st=0, col_st=0):
     """ Calculate the average brightness of an image.
 
     Args:
@@ -119,9 +126,11 @@ def avg_brightness_calculator(pix, w, h):
     """
     brightness_avg = 0
     img_size = w*h
-    for i in range(w):
-        for j in range(h):
+    for i in range(row_st, row_st + h):
+        for j in range(col_st, col_st + w):
+            # print(i,j)
             brightness_avg += avg_brightness_pixel(pix[i,j]) / img_size
+    # print("---")
     return brightness_avg
 
 def get_images_filenames(images_folder, extension):
@@ -144,7 +153,7 @@ def get_images_filenames(images_folder, extension):
                 result.append(i)
     return result
 
-def generate_table(images_folder, out_file, extension="png", precision=4, meta_file_name="meta.txt"):
+def generate_table(images_folder, out_file, extension="png", precision=4, meta_file_name="meta.txt", scaling_optimization=True):
     """Generate the brightness table used in conversion. 
        TODO: Maybe create a K-D tree
     
@@ -154,6 +163,7 @@ def generate_table(images_folder, out_file, extension="png", precision=4, meta_f
         extension (str, optional): Extension of files to process
         precision (int, optional): Floating point precision to be kept
         meta_file_name (str, optional): name of meta data file in images folder
+        scaling_optimization (bool, optional): Perform the scaling optimization during table generation.
     
     Raises:
         Exception: Invalid meta data object
@@ -169,6 +179,7 @@ def generate_table(images_folder, out_file, extension="png", precision=4, meta_f
     hashtable = {}
     minval = None
     maxval = None
+    w,h = None, None
     print("Generating table")
     for i in images:
         ascii_code = meta_data[FILE2I_KEY][i]
@@ -185,18 +196,109 @@ def generate_table(images_folder, out_file, extension="png", precision=4, meta_f
             minval = avg_brightness if minval==None else min(minval, avg_brightness)
             maxval = avg_brightness if maxval==None else max(maxval, avg_brightness)
     
-    result[TABLE_KEY] = hashtable
+    # TODO: handle None values.
+    scaled_hashtable = {}
+    if scaling_optimization:
+        for i in hashtable:
+            scaled_avg_brightness = 255 * (i - minval) / (maxval-minval)
+            scaled_hashtable[scaled_avg_brightness] = hashtable[i]
+    else:
+        scaled_hashtable = hashtable
+
+    result[TABLE_KEY] = scaled_hashtable
     result[STATS_KEY] = {}
+    # keeping the original min and max vals in case it is required to reverse the op.
     result[STATS_KEY][MIN_VAL_KEY] = minval
     result[STATS_KEY][MAX_VAL_KEY] = maxval
     result[META_KEY] = {}
     result[META_KEY][DATA_FOLDER_KEY] = images_folder
+    result[META_KEY][IMAGE_WIDTH_KEY], result[META_KEY][IMAGE_HEIGHT_KEY] = w, h
 
     print("writing table to:",out_file)    
     with open(out_file, "w") as f:
         f.write(json.dumps(result, indent=2))
         f.write("\n")
     print("done")
+
+def get_closest_chr_code(table, search_val):
+    idx = None
+    code = None
+    search_val = float(search_val)
+    for i in table:
+        curr_value = float(i)
+        if idx==None or abs(curr_value - search_val) < abs(idx - search_val):
+            idx = curr_value
+            code = table[i]
+    return int(code)
+
+
+def img2Ascii(pixels, table, img_width, img_height, chr_width, chr_height, op_width=None, op_height=None):
+    """Convert an image into ASCII art.
+    
+    Args:
+        pixels (pixel[][]): Image that is to be converted.
+        table (dict): A table to lookup ascii characters based on brightness
+        img_width (int): width of input image in pixels
+        img_height (int): height of input image in pixels
+        chr_width (int): chr width pixels
+        chr_height (int): chr height pixels
+        op_width (int): output width in number of columns
+        op_height (int): output height in number of rows
+    
+    Returns:
+        str[][]: The matrix of ascii chars
+    """
+    result_width = max(img_width // chr_width, 1)
+    result_height = max(img_height // chr_height, 1)
+    result=[]
+    # print("Input is {}x{} px and each char is {}x{} px".format(img_width, img_height, chr_width, chr_height))
+    # print("output will be {}x{}".format(result_width, result_height))
+    for i in range(result_height):
+        tmp=[]
+        for j in range(result_width):
+            # Theory:
+            # row_st must be according to i and col_st must be according to j if we consider pixels a matrix.
+            # But the way PIL.PixelAccess works is (i,j) returns (width+i, height+j)
+            row_st = j * chr_height
+            col_st = i * chr_width
+            img_patch_brightness = avg_brightness_calculator(pixels, chr_width, chr_height, row_st, col_st)
+            closest = get_closest_chr_code(table, img_patch_brightness)
+            tmp.append(chr(closest).encode("latin-1").decode("latin-1"))
+        result.append(tmp)
+    return result
+
+def convert_and_print(img_file, table_path, op_size):
+    """Convert image file into ASCII art and print it.
+    
+    Args:
+        img_file (str): path to image file.
+        table_path (str): path to the stat table.
+        op_size (int): Output scaling. It works as follows; 
+                        - The number of columns will be (img_width (px) * op_size) / character_width (px)
+                        - The number of rows will be calculated similarly using heights.
+                        Hence more the op_size, the bigger the image will be
+    """ 
+    
+    # load up the table and the image
+    table_obj = {}
+    with open(table_path,"r") as f:
+        try:
+            table_obj = json.load(f)
+        except ValueError:
+            print("problem parsing table JSON, aborting...")
+            return
+    image = Image.open(img_file)
+    w,h = image.size
+    pixels = image.load()
+
+    # Calculate character widths after scaling
+    chr_width, chr_height = table_obj[META_KEY][IMAGE_WIDTH_KEY], table_obj[META_KEY][IMAGE_HEIGHT_KEY]
+    chr_width = ceil(chr_width / op_size)
+    chr_height = ceil(chr_height / op_size)
+    # do the conversion and print it
+    result = img2Ascii(pixels, table_obj[TABLE_KEY], w, h, chr_width, chr_height)
+    for i in result:
+        print("".join(i))
 
 
 if __name__ == "__main__":
@@ -205,12 +307,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--img", help="Image file to convert", default=None)
     parser.add_argument("--font-path", help="Font file", default=None)
-    parser.add_argument("--font-size", help="Font size in pt", default=48, type=int)
-    parser.add_argument("--bitmap-size", help="Size of font bitmap", default=56, type=int)
+    parser.add_argument("--font-size", help="Font size in pt", default=20, type=int)
+    parser.add_argument("--bitmap-size", help="Size of font bitmap", default=20, type=int)
     parser.add_argument("--bitmap-out-dir", help="output directory for writing font bitmaps", default="font-bitmaps")
     parser.add_argument("--data-dir", help="directory containing character image files")
     parser.add_argument("--ext", help="Extension of character image files", default="png")
     parser.add_argument("--table-out", help="table output file", default="table.txt")
+    parser.add_argument("--no-optimization", help="dont use scaling optimization, -_-", action="store_true")
+    parser.add_argument("--convert-img", help="image to convert", default=None)
+    parser.add_argument("--table-path", help="table to use", default=None)
+    parser.add_argument("--output-size", help="output size of ASCII art, more the number, bigger it will be", type=int, default=7)
 
     args = parser.parse_args()
 
@@ -222,6 +328,13 @@ if __name__ == "__main__":
     data_dir = args.data_dir
     ext = args.ext
     table_out = args.table_out
+    scaling_optimization = not args.no_optimization
+    convert_img = args.convert_img
+    table_path = args.table_path
+    output_size = args.output_size
+
+    if convert_img!=None and table_path!=None:
+        convert_and_print(convert_img, table_path, output_size)
 
 
     if not os.path.exists(bitmap_out_dir):
@@ -229,7 +342,7 @@ if __name__ == "__main__":
 
     if data_dir:
         data_dir = os.path.abspath(data_dir)
-        generate_table(data_dir, table_out, extension=ext, precision=4) 
+        generate_table(data_dir, table_out, extension=ext, precision=4, scaling_optimization=scaling_optimization) 
 
     elif font_file:
         generate_images_for_charset(font_file, font_size, img_dimensions, bitmap_out_dir)
